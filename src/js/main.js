627a039e834b9685047a045c483463e9a8b5673d
hermitoff <tomgimenez2006@outlook.fr>
2023-12-19 16:17:46 +1100

    English translation

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,8 @@ window.addEventListener('DOMContentLoaded', () => {
       return data.ip;
     })
     .catch(error => {
-      console.error('Erreur lors de la récupération de l\'adresse IPv4 :', error);
-      ipv4Span.textContent = 'Erreur lors de la récupération';
+      console.error('Error while retrieving IPv4:', error);
+      ipv4Span.textContent = 'Error during retrieval';
     });
 
   const ipv6Promise = fetch('https://api64.ipify.org?format=json')
@@ -22,24 +22,24 @@ window.addEventListener('DOMContentLoaded', () => {
       return data.ip;
     })
     .catch(error => {
-      console.error('Erreur lors de la récupération de l\'adresse IPv6 :', error);
-      ipv6Span.textContent = 'Erreur lors de la récupération';
+      console.error('Error while retrieving IPv6:', error);
+      ipv6Span.textContent = 'Error during retrieval';
     });
 
   Promise.all([ipv4Promise, ipv6Promise])
     .then(([ipv4, ipv6]) => {
       if (ipv4 === ipv6) {
         // Les adresses IPv4 et IPv6 sont égales, affiche une erreur
-        console.error("Impossible d'obtenir l'IPv6 :", ipv4);
+        console.error("Error while retrieving IPv6:", ipv4);
         
         // Vérifie si l'élément 'erreur' existe avant de définir son contenu
         const erreurSpan = document.getElementById('ipv6');
         if (erreurSpan) {
-          erreurSpan.textContent = "Impossible d'obtenir l'IPv6.";
+          erreurSpan.textContent = "Impossible to get IPv6";
         }
       }
     })
     .catch(error => {
-      console.error('Erreur lors de la comparaison des adresses IPv4 et IPv6 :', error);
+      console.error('Error comparing IPv4 and IPv6 addresses:', error);
     });
 });
